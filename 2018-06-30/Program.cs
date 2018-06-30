4619642d2c0cf1316927fa56d85ac9a50c4425a6
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-06-30 15:36:59 -0400

    End of Stream Commit

---
+++ 2018-06-30/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _2018_06_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i < 10; i++)
+            {                
+                if (i == 4)
+                {
+                    Console.WriteLine("This one is four!");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            // Random rand = new Random();
+            // int number = rand.Next(1, 100);
+            // Console.WriteLine("I've chosen a number between 1 and 1,000! Can you guess it?!?!");
+
+            // int guess;
+            // do
+            // {
+            //     Console.WriteLine("What's your guess?!");
+            //     string guessString = Console.ReadLine();
+            //     int.TryParse(guessString, out guess);
+            //     if (guess < number)
+            //     {
+            //         Console.WriteLine("You guessed too low. Guess higher next time.");
+            //     }
+            //     if (guess > number)
+            //     {
+            //         Console.WriteLine("You guessed too high. Guess lower next time.");
+            //     }
+
+            // } while (guess != number);
+
+            // Console.WriteLine("You got it!");
+        }
+    }
+}
