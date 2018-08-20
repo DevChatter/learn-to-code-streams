8b1d98f061fb01f72f00a75340a4813ac7fc509e
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-08-20 17:17:49 -0400

    End of stream commit

---
+++ 2018-08-20/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2018_08_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string>();
+            string nameToAdd;
+            do
+            {
+                Console.WriteLine("Please enter a name: ");
+                nameToAdd = Console.ReadLine();
+                names.Add(nameToAdd);
+            } while (!string.IsNullOrWhiteSpace(nameToAdd));
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("We left the loop");
+        }
+    }
+}
